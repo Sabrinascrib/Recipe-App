cafa1ef557c934ce8d52b03750b49ddbd38863c0
sabrina <sabrinaabrodi20@gmail.com>
2022-06-19 07:01:19 +0200

    adding let keywords

---
+++ app.js
@@ -0,0 +1,4 @@
+//Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
